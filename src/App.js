374a1bef74ef9dbcfacbbb40298e3711df4ab869
GUNA SGS <gunasgs357@gmail.com>
2022-07-02 16:22:56 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
-function App() {
+import { Login } from "./components";
+import Home from "./container/Home";
+import { fetchUser } from "./utils/fetchUser";
+
+const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User = fetchUser();
+
+    if (!User) navigate("/login");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   );
-}
+};
 
 export default App;
